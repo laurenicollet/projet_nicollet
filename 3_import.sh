adabb1cebf23d300e41839e3286803fcd3e861ab
laurenicollet <laure.nicollet@eleve.ensai.fr>
2017-02-21 18:23:15 +0100

    Update 3_import.sh

--- 3_import.sh
+++ 3_import.sh
@@ -1,7 +1,5 @@
-mongoimport -d "new_york" -c "subways" --type json --file "projet_nicollet/data/subways.geojson" --jsonArray
+mongoimport -d "new_york" -c "subways" --type json --file "data/subways.geojson" --jsonArray
 
+mongoimport -d "new_york" -c "parks" --type json --file "data/parks.geojson" --jsonArray
 
-mongoimport -d "new_york" -c "parks" --type json --file "projet_nicollet/data/parks.geojson" --jsonArray
-
-mongoimport -d "new_york" -c "censusBlocks" --type json --file "projet_nicollet/data/censusBlocks.geojson" --jsonArray
-
+mongoimport -d "new_york" -c "censusBlocks" --type json --file "data/censusBlocks.geojson" --jsonArray
